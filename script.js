93681b471a6c3ef2b537fee72680a32e6409bf5b
pinky <pinky.fardous@gmail.com>
2021-09-20 11:35:04 +0200

    my third commit

--- script.js
+++ script.js
@@ -1,36 +1,24 @@
 // var myBtn1 = document.getElementById("btn1");
 // var myBtn2 = document.getElementById("btn2");
-var myLight = document.getElementById("light");
+var lightImage = document.getElementById("light");
 const toggleBtn = document.getElementById('toggle');
-var changeSentence = document.getElementsByTagName("h1")[0];
+var changeHeadline = document.getElementsByTagName("h1")[0];
 let lightState = 'off';
 
-// function myFunction1() {
-//     myLight.src="lightOn.jpg";
-//     changeSentence.innerHTML = "Off the light";
-//     changeSentence.setAttribute("style","color:red");
-// }
-
-// function myFunction2() {
-//     myLight.src="lightOff.jpg";
-//     changeSentence.innerHTML = "On the light";
-//     changeSentence.setAttribute("style","color:blue");
-// }
-
-// myBtn1.addEventListener("click",myFunction1);
-// myBtn2.addEventListener("click",myFunction2);
-
 function turnOn(){
-    myLight.src="lightOff.jpg";
-    changeSentence.innerHTML = "On the light";
+    lightImage.src="lightOff.jpg";
+    changeHeadline.innerHTML = "On the light";
     changeSentence.setAttribute("style","color:blue")
 }
 
 function turnOff() {
-    myLight.src="lightOn.jpg";
-    changeSentence.innerHTML = "Off the light";
+    lightImage.src="lightOn.jpg";
+    changeHeadline.innerHTML = "Off the light";
     changeSentence.setAttribute("style","color:red")
 }
+
+// myBtn1.addEventListener("click",turnOn);
+// myBtn2.addEventListener("click",turnOff);
 
 function toggleLight() {
     if(lightState === 'on') {

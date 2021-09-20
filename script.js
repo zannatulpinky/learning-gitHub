d523087f8a4d6e09ef927dad3575a3a5653d3489
pinky <pinky.fardous@gmail.com>
2021-09-20 15:18:34 +0200

    change button innerHtml

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 // var myBtn1 = document.getElementById("btn1");
 // var myBtn2 = document.getElementById("btn2");
+var changeHeadline = document.getElementsByTagName("h1")[0];
 var lightImage = document.getElementById("light");
 const toggleBtn = document.getElementById('toggle');
-var changeHeadline = document.getElementsByTagName("h1")[0];
 let lightState = 'off';
 
+//Function for light on and light off.
 function turnOn(){
     lightImage.src="images/lightOff.jpg";
     changeHeadline.innerHTML = "On the light";
-    changeHeadline.setAttribute("style","color:blue")
+    changeHeadline.setAttribute("style","color:blue");
+    toggleBtn.innerHTML = "On";
 }
 
 function turnOff() {
     lightImage.src="images/lightOn.jpg";
     changeHeadline.innerHTML = "Off the light";
-    changeHeadline.setAttribute("style","color:red")
+    changeHeadline.setAttribute("style","color:red");
+    toggleBtn.innerHTML = "Off";
 }
 
 // myBtn1.addEventListener("click",turnOn);
 // myBtn2.addEventListener("click",turnOff);
 
+//use logical operator for using one button.
 function toggleLight() {
     if(lightState === 'on') {
         lightState = 'off';

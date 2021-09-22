588e510dd58f9b374069e01e106f01b6e5003f86
pinky <pinky.fardous@gmail.com>
2021-09-22 13:48:58 +0200

    add new lights

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 // var myBtn1 = document.getElementById("btn1");
 // var myBtn2 = document.getElementById("btn2");
 var changeHeadline = document.getElementsByTagName("h3")[0];
-var lightImage1 = document.getElementsByClassName("light")[0];
-var lightImage2 = document.getElementsByClassName("light")[1];
-var lightImage3 = document.getElementsByClassName("light")[2];
-var lightImage = [lightImage1,lightImage2,lightImage3];
+var lightImage1 = document.getElementsByClassName("light1")[0];
+var lightImage2 = document.getElementsByClassName("light2")[1];
+var lightImage3 = document.getElementsByClassName("light3")[2];
 const toggleBtn = document.getElementById('toggle');
 let lightState = 'off';
 
 //Function for light on and light off.
+    
 function turnOn(){
-    lightImage1.src="images/lightOff.jpg";
-    lightImage2.src="images/lightOff.jpg";
-    lightImage3.src="images/lightOff.jpg";
+    lightImage1.src = "images/lightOn.jpg";
     changeHeadline.innerHTML = "On the light";
     changeHeadline.setAttribute("style","color:blue");
     toggleBtn.innerHTML = "On";
 }
 
 function turnOff() {
-    lightImage1.src="images/lightOn.jpg";
-    lightImage2.src="images/lightOn.jpg";
-    lightImage3.src="images/lightOn.jpg";
+    lightImage1.src = "images/lightOff.jpg";
     changeHeadline.innerHTML = "Off the light";
     changeHeadline.setAttribute("style","color:red");
     toggleBtn.innerHTML = "Off";
@@ -31,8 +27,8 @@ function turnOff() {
 // myBtn2.addEventListener("click",turnOff);
 
 //use logical operator for using one button.
-function toggleLight() {
-    if(lightState === 'on') {
+function toggleLight() {        
+     if(lightState === 'on') {
         lightState = 'off';
         turnOff()
     } else {
